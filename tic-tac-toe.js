afd047181230f3ccb6858528bd7e69bfe57bf907
Rashede Callum <rcallum461@gmail.com>
2021-10-09 13:36:42 -0500

    Javascript file

---
+++ tic-tac-toe.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+
+
+    document.querySelector("#board div:nth-of-type(1)").setAttribute("class", "square"); 
+    document.querySelector("#board div:nth-of-type(2)").setAttribute("class", "square");
+    document.querySelector("#board div:nth-of-type(3)").setAttribute("class", "square");
+    document.querySelector("#board div:nth-of-type(4)").setAttribute("class", "square");
+    document.querySelector("#board div:nth-of-type(5)").setAttribute("class", "square");
+    document.querySelector("#board div:nth-of-type(6)").setAttribute("class", "square");
+    document.querySelector("#board div:nth-of-type(7)").setAttribute("class", "square");
+    document.querySelector("#board div:nth-of-type(8)").setAttribute("class", "square");
+	document.querySelector("#board div:nth-of-type(9)").setAttribute("class", "square");
+	
+
+}
